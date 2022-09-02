c314ee5e4e4eb9d077598796bae6c53c3117e987
munjihye <m84422583@gmail.com>
2022-09-02 22:17:46 +0900

    09.02

--- frontend/kbsc/src/screens/FromText.js
+++ frontend/kbsc/src/screens/FromText.js
@@ -1,66 +1,77 @@
 import { Header } from '../components/Styles/Header/Header.styled'
-//import { Container } from '../components/Styles/Container/Container.style'
+import { Container } from '../components/Styles/Container/Container.style'
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
 import React , { useRef } from 'react';
 import { Box } from '@mui/system';
 import { Text } from '../components/Styles/Container/Camera.style';
-import { Container } from '../components/Styles/Container/Container.style';
+import { useState, useEffect } from 'react';
 
 
 export default function FromText() {
-  const textRef = useRef(null);
+const textRef = useRef(null);
 
-  const onSubmit=(e)=>{
-    console.log(textRef.current.value)
-  };
 
-  function update(){
-    fetch(`http://localhost:3001/text`,{
-      method:'POST',
-      headers:{
-        'Content-Type' : 'application/json'
-      },
-      body : JSON.stringify({
-        text : textRef.current.value,
-      }),
-    })
-    .then(res => {
-      if(res.ok){
-        alert('생성이 완료되었습니다.')
-      }
-    })
-  }
-
-  return (
-    <Container>
-      <Header>
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-      <TextField 
-      margin = "normal"
-      label="text" 
-      name = "text" 
-      autoFocus
-      style={{width:'350%'}}
-      inputRef={textRef}
-      />
-      <Button onClick={update}>번역</Button>
-      </Box>
-      </Header>
-      <Text>
-      WordList
-      </Text>
-    </Container>
-    
-
-  );
+function update(){
+  fetch(`http://localhost:3001/text`,{
+    method:'POST',
+    headers:{
+      'Content-Type' : 'application/json'
+    },
+    body : JSON.stringify({
+      text : textRef.current.value,
+    }),
+  })
+  .then(res => {
+    if(res.ok){
+      alert('생성이 완료되었습니다.')
+    }
+  })
 }
 
+const [words, setWords] = useState([]);
 
+useEffect(() => {
+  fetch("http://localhost:3001/wordList")
+    .then(res=>{
+      return res.json();
+    })
+    .then(data =>{
+      setWords(data);
+    });
+
+}, []);
+
+return (
+  <Container>
+    <Header>
+    <Box
+      sx={{
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+    <TextField 
+    margin = "normal"
+    label="text" 
+    name = "text" 
+    autoFocus
+    style={{width:'350%'}}
+    inputRef={textRef}
+    />
+    <Button onClick={update}>번역</Button>
+    </Box>
+    </Header>
+    <Text>
+    {words.map(word => (
+      <li key={word.id}>
+        <div className='word'>
+          {word.word} : {word.text}
+        </div>
+      </li>
+    ))}
+    </Text>
+  </Container>
+)}
